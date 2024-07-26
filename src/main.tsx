70f113723508c459f25292317d4f5ebf3288b121
Joselu <joseluissgz97@gmail.com>
2024-07-26 23:32:07 +0200

    fix: remove console.log

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,17 @@
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { App } from "./App";
-import "./index.css";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './App';
+import './index.scss';
 
 const clienID = import.meta.env.VITE_ID_CLIENT;
-console.log({ clienID });
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<GoogleOAuthProvider clientId={clienID}>
-				<App />
-			</GoogleOAuthProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <GoogleOAuthProvider clientId={clienID}>
+        <App />
+      </GoogleOAuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
