3813d35e9c99db6eb95aaa6ba24bdfaea5bb0d21
Joselu <joseluissgz97@gmail.com>
2024-07-26 23:31:21 +0200

    feat: Add AuthProvider component for managing user authentication

---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,37 @@
+import { useReducer } from 'react';
+import { UserInterface } from '../interface/user.interface';
+import { AuthContext } from './AuthContext';
+import { authReducer, AuthReducerState } from './authReducer';
+
+type AuthProviderProps = {
+  children: React.ReactNode;
+};
+
+const initialState: AuthReducerState = {
+  isLooged: false,
+  user: null,
+};
+const init = (): AuthReducerState => {
+  const userString = localStorage.getItem('userData');
+  const userData = userString ? JSON.parse(userString) : null;
+
+  return {
+    isLooged: !!userData,
+    user: userData,
+  };
+};
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [authState, dispatch] = useReducer(authReducer, initialState, init);
+
+  const onLogin = (userData: UserInterface) => {
+    localStorage.setItem('userData', JSON.stringify(userData));
+    dispatch({
+      type: 'signIn',
+      payload: userData,
+    });
+  };
+  const onLogout = () => {};
+
+  return <AuthContext.Provider value={{ onLogin, onLogout, authState }}>{children}</AuthContext.Provider>;
+};
