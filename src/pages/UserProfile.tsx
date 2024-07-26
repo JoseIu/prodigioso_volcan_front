5577f9c094fd7cfc9b49b43813c836c806e3b192
Joselu <joseluissgz97@gmail.com>
2024-07-26 23:31:43 +0200

    feat: Update UserProfile to display user information and user card component

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,3 +1,14 @@
+import { UserCard } from '../components/UserCard';
+import { useAuth } from '../hook/useAuth';
+
 export const UserProfile = () => {
-	return <div>UserProfile</div>;
+  const { authState } = useAuth();
+
+  console.log(authState.user);
+  return (
+    <section className="wrapper">
+      <h1>User Profile: {authState.user?.name}</h1>
+      <UserCard user={authState.user!} />
+    </section>
+  );
 };
