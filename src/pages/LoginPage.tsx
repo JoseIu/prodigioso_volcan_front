8fcfc4cf3565d1cc11fa370825ba4406b8340e50
Joselu <joseluissgz97@gmail.com>
2024-07-26 23:31:36 +0200

    feat: Update LoginPage to handle successful login and navigate to user profile

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,15 +1,28 @@
-import { GoogleLogin } from "@react-oauth/google";
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import { fetchApi } from '../api/fetchApi';
+import { useAuth } from '../hook/useAuth';
 export const LoginPage = () => {
-	return (
-		<div>
-			<GoogleLogin
-				onSuccess={(credentialResponse) => {
-					console.log(credentialResponse);
-				}}
-				onError={() => {
-					console.log("Login Failed");
-				}}
-			/>
-		</div>
-	);
+  const { onLogin } = useAuth();
+  const navigate = useNavigate();
+
+  const onHandleSuccess = async (credentialResponse: CredentialResponse) => {
+    const { credential } = credentialResponse;
+    const dataUser = await fetchApi(credential!);
+
+    if (dataUser.error) {
+      console.log(dataUser.message);
+      return;
+    }
+    const { user } = dataUser;
+    onLogin(user);
+    navigate('/user-profile');
+  };
+
+  const onHandleError = () => {};
+  return (
+    <div>
+      <GoogleLogin onSuccess={onHandleSuccess} onError={onHandleError} />
+    </div>
+  );
 };
