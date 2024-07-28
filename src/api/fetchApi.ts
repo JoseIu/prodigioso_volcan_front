20b3227446366a9cf0cffbbe147f7e7b3421d6d4
Joselu <joseluissgz97@gmail.com>
2024-07-28 21:11:41 +0200

    refactor: Update fetchApi to support dynamic path and method parameters

--- src/api/fetchApi.ts
+++ src/api/fetchApi.ts
@@ -1,18 +1,28 @@
 import { APIResponseInterface } from '../interface/apiResponse.interface';
 
-export const fetchApi = async (credential: string): Promise<APIResponseInterface> => {
+export const fetchApi = async (
+  path: string,
+  method: string = 'GET',
+  credential?: string | null,
+  code?: string | null
+): Promise<APIResponseInterface> => {
   const BACK_END_URL = import.meta.env.VITE_BACK_URL;
 
-  const fetchOptions = {
-    method: 'POST',
+  const fetchOptions: RequestInit = {
+    method: method,
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${credential}`,
     },
   };
 
-  const response = await fetch(`${BACK_END_URL}/login-google`, fetchOptions);
+  if (code) {
+    fetchOptions.body = JSON.stringify({ code });
+  }
+
+  const response = await fetch(`${BACK_END_URL}${path}`, fetchOptions);
 
   const data: APIResponseInterface = await response.json();
+
   return data;
 };
