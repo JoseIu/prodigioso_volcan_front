e5b8311e58f9b1811d4a4484e9cae71bbd6761ce
Joselu <joseluissgz97@gmail.com>
2024-07-28 21:12:44 +0200

    add: googleLogin V2

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
+import { CredentialResponse, GoogleLogin, useGoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
 import { fetchApi } from '../api/fetchApi';
 import { useAuth } from '../hook/useAuth';
@@ -8,7 +8,7 @@ export const LoginPage = () => {
 
   const onHandleSuccess = async (credentialResponse: CredentialResponse) => {
     const { credential } = credentialResponse;
-    const dataUser = await fetchApi(credential!);
+    const dataUser = await fetchApi('/login-google', 'POST', credential!);
 
     if (dataUser.error) {
       console.log(dataUser.message);
@@ -18,11 +18,26 @@ export const LoginPage = () => {
     onLogin(user);
     navigate('/user-profile');
   };
+  const googleLogin = useGoogleLogin({
+    flow: 'auth-code',
+    onSuccess: async (tokenResponse) => {
+      const dataUser = await fetchApi('/login-googlev2', 'POST', '', tokenResponse.code);
+      if (dataUser.error) {
+        console.log(dataUser.message);
+        return;
+      }
+      const { user } = dataUser;
+      onLogin(user);
+      navigate('/user-profile');
+    },
+    onError: (errorResponse) => console.log(errorResponse),
+  });
 
   const onHandleError = () => {};
   return (
     <div>
       <GoogleLogin onSuccess={onHandleSuccess} onError={onHandleError} />
+      <button onClick={googleLogin}>Login with Google</button>
     </div>
   );
 };
